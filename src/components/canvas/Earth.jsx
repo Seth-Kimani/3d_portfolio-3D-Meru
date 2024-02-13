30732bab5ce20114b88a3dad96f0bb9907964fd8
Seth-Kimani <sethkimani99@gmail.com>
2024-02-13 17:00:23 +0300

    3d_models

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -8,7 +8,7 @@ const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={5} />
   );
 };
 
@@ -30,8 +30,7 @@ const EarthCanvas = () => {
         <OrbitControls
           autoRotate
           enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
+          
         />
         <Earth />
 
