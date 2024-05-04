abd9f99e31e60c4314e5748806886e60bfb2cf79
Seth Kimani <43470580+Seth-Kimani@users.noreply.github.com>
2024-05-04 17:36:42 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Marcus Kiogora <br>(3D Meru)</span>
+            Hi, I'm <span className='text-[#915EFF]'>Marcus Kiogora</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
